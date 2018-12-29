18e2daa8877f19e45a8a8bacb93f8c19c1dfd782
Peïo THIBAULT <peio.thibault@gmail.com>
2018-12-30 00:15:15 +0100

    test: getDonations method

--- src/test/java/com/github/twitch4j/streamlabs4j/api/StreamlabsApiTest.java
+++ src/test/java/com/github/twitch4j/streamlabs4j/api/StreamlabsApiTest.java
@@ -1,10 +1,12 @@
 package com.github.twitch4j.streamlabs4j.api;
 
+import com.github.twitch4j.streamlabs4j.api.domain.StreamlabsDonationsData;
 import com.github.twitch4j.streamlabs4j.api.domain.StreamlabsUser;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class StreamlabsApiTest {
 
@@ -29,4 +31,37 @@ class StreamlabsApiTest {
         assertEquals("YaourtGG", user.getTwitch().get().getDisplayName());
         assertEquals("yaourtgg", user.getTwitch().get().getName());
     }
+
+    @Test
+    public void getDonationsTest() {
+        StreamlabsDonationsData donations = api.getDonations(authToken, 100, "", "", "", "").execute();
+
+        assertNotEquals(0, donations.getDonations().size());
+
+        assertTrue(donations.getDonations()
+            .stream()
+            .anyMatch(donation -> donation.getName().equals("yaourtgg")),
+            "yaourtgg has donated at least once");
+
+        assertTrue(donations.getDonations()
+            .stream()
+            .anyMatch(donation -> donation.getName().equals("Hexafice")),
+            "Hexafice has donated at least once");
+
+        assertTrue(donations.getDonations()
+            .stream()
+            .anyMatch(donation -> donation.getCurrency().equals("EUR")),
+            "At least one donation in EUR");
+
+        assertTrue(donations.getDonations()
+            .stream()
+            .anyMatch(donation -> donation.getMessage().isPresent()),
+            "One of the donations has a message");
+
+        assertTrue(donations.getDonations()
+            .stream()
+            .anyMatch(donation -> donation.getMessage().equals(Optional.empty())),
+            "One of the donations doesn't have a message");
+    }
+
 }
